c13a1050cf9ab6b7b3b7a279fdf93fe8b814766f
Ed <eduardo_casillas@hotmail.com>
2022-11-28 16:49:05 -0800

    Added colors for the circles app

--- main.cpp
+++ main.cpp
@@ -22,25 +22,46 @@ void post_method_handler( const shared_ptr< Session > session ){
     int content_length = request->get_header( "Content-Length", 0 );
 
     session->fetch( content_length, [ ]( const shared_ptr< Session > session, const Bytes & body ) {
-        session->close( OK, "Hello, World!", { { "Content-Length", "13" } } );
+        // body can be read here
+        session->close( OK, "POSTED!", { { "Content-Length", "7" } } );
     } );
 }
 
-void get_method_handler(const shared_ptr<Session> session) {
-   auto content = to_json(98);
-   session->close(OK, content, 
+void close_session_with_result(const shared_ptr<Session> session, const string content) {
+    session->close(OK, content, 
         {{"Content-Length", to_string(content.size())}});
 }
 
+void get_method_handler(const shared_ptr<Session> session) {
+    close_session_with_result(session, to_json(98));
+}
+
+string get_circles_data_json() { 
+    nlohmann::json jsonResult = {
+        {"default-circle-color", "brown"},
+        {"last-circle-color", "green"},
+        {"greeting", "Hello from the backend!"}
+    };
+    return jsonResult.dump();
+}
+
+void get_circles_data(const shared_ptr<Session> session) {
+    close_session_with_result(session, get_circles_data_json());
+}
+
 int main( const int, const char** ) {
-    auto resource = make_shared< Resource >( );
+    auto resource = make_shared<Resource>( );
     resource->set_path( "/resource" );
     resource->set_method_handler( "POST", post_method_handler );
     resource->set_method_handler( "GET", get_method_handler );
 
-    auto another_resource = make_shared< Resource >( );
+    auto another_resource = make_shared<Resource>( );
     another_resource->set_path( "/another_resource" );
     another_resource->set_method_handler( "GET", get_method_handler );
+
+    auto circles_resource = make_shared<Resource>();
+    circles_resource->set_path("/circles");
+    circles_resource->set_method_handler("GET", get_circles_data);
 
     auto settings = make_shared< Settings >( );
     settings->set_port( 1984 );
@@ -50,6 +71,7 @@ int main( const int, const char** ) {
     Service service;
     service.publish( resource );
     service.publish(another_resource);
+    service.publish(circles_resource);
 
     cout << "Service started";
 
